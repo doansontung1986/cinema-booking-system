a08f2db734401fc4ccfd041020e62376c3606563
Tung Doan <doansontung2608@gmail.com>
2023-11-11 22:31:17 +0700

    Added all files

---
+++ queries.sql
@@ -0,0 +1,16 @@
+SELECT * FROM films;
+SELECT * FROM customers;
+SELECT * FROM rooms;
+SELECT * FROM screenings;
+SELECT * FROM seats;
+SELECT * FROM bookings;
+SELECT * FROM reserved_seat;
+
+SELECT *, CONCAT_WS('-',seat_row, number) as 'Seat Code'
+FROM films
+INNER JOIN screenings
+ON films.id = screenings.film_id
+INNER JOIN rooms
+ON rooms.id = screenings.room_id
+INNER JOIN seats
+ON seats.room_id = rooms.id;
