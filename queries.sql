65dcaa574a81220e60d134cf0ad3f2c305b738b3
Tung Doan <doansontung2608@gmail.com>
2023-11-16 20:29:25 +0700

    Add create_ar fields and Accounts table

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 SELECT * FROM films;
-SELECT * FROM customers;
+SELECT * FROM accounts;
 SELECT * FROM rooms;
 SELECT * FROM screenings;
 SELECT * FROM seats;
@@ -10,11 +10,17 @@ SELECT * FROM categories;
 SELECT * FROM actor_film;
 SELECT * FROM film_category;
 
-SELECT *, CONCAT_WS('-',seat_row, number) as 'Seat Code'
-FROM films
+SELECT accounts.id, accounts.first_name, accounts.last_name, CONCAT(seats.seat_row, seats.number) as 'seat', screenings.start_time
+FROM accounts
+INNER JOIN bookings
+ON accounts.id = bookings.account_id
 INNER JOIN screenings
-ON films.id = screenings.film_id
+ON screenings.id = bookings.screening_id
 INNER JOIN rooms
 ON rooms.id = screenings.room_id
 INNER JOIN seats
-ON seats.room_id = rooms.id;
+ON seats.room_id = rooms.id
+INNER JOIN reserved_seat
+ON reserved_seat.seat_id = seats.id
+WHERE accounts.id = 2;
+
